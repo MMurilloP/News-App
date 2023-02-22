5d6a9230297ff6b418905c5831b981ab68d486ad
Manuel Murillo <mmpeira@gmail.com>
2023-02-22 20:49:22 +0100

    estilos terminados

--- src/componets/Main/Form/Form.jsx
+++ src/componets/Main/Form/Form.jsx
@@ -1,21 +1,51 @@
 import React, { Component } from 'react'
+import Button from "@mui/material/Button";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import '../../../styles/styles.css'
 
 export class Form extends Component {
 
   render() {
     return (
-      <div>
+      <div className='form-container'>
         <h1>CREA UNA NUEVA NOTICIA:</h1>
-        <form action="">
-          <label htmlFor="">Titular</label>
-          <input type="text" placeholder='Introduce el titular de la noticia'/>
-          <label htmlFor="">Descripcion</label>
-          <input type="text" placeholder='Describe brevemente la noticia'/>
-          <label htmlFor="">Url</label>
-          <input type="text" placeholder='Introduce la URL de la noticia'/>
-          <input type="submit" />
-        </form>
-        
+        <Box
+          component="form"
+          sx={{
+            '& > :not(style)': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+            <TextField
+              id="standard-basic"
+              label="Titular"
+              variant="standard"
+              className="form-textfield"
+            />
+            <TextField
+              id="standard-basic"
+              label="Noticia"
+              variant="standard"
+              className="form-textfield"
+              multiline
+              rows={4}
+            />
+            <TextField
+              id="standard-basic"
+              label="Url"
+              variant="standard"
+              className="form-textfield"
+            />
+            <Button
+              variant="contained"
+              className="css-button-sliding-to-left--red"
+              onClick={""}
+            >
+              CREAR NOTICIA
+            </Button>
+        </Box>  
       </div>
     )
   }
--- src/componets/Main/Home/Home.jsx
+++ src/componets/Main/Home/Home.jsx
@@ -1,13 +1,39 @@
 import React, { Component } from "react";
+import Button from "@mui/material/Button";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import '../../../styles/styles.css'
+
 
 class Home extends Component {
   render() {
-    return <div>
-      <h1>BIENVENIDO A NEWS-APP</h1>
-      <h2>Introduce tu nombre:</h2>
-      <input type="text"></input>
-      <input type="submit" value="Enviar"/>
-      </div>;
+    return (
+      <div className="home-container">
+        <h1>BIENVENIDO A NEWS-APP</h1>
+        <Box
+          component="form"
+          sx={{
+            '& > :not(style)': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            id="standard-basic"
+            label="Introduce tú nombre"
+            variant="standard"
+            className="form-textfield"
+          />
+          <Button
+            variant="contained"
+            className="css-button-sliding-to-left--red"
+            onClick={""}
+          >
+            LOGIN
+          </Button>
+        </Box>
+      </div>
+    );
   }
 }
 
