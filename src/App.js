cb670e9caf4b801908b3011bd36de20a6edc1eab
Manuel Murillo <mmpeira@gmail.com>
2023-02-23 17:31:54 +0100

    login funciona ok

--- src/App.js
+++ src/App.js
@@ -2,15 +2,32 @@ import './App.css';
 import Header from './componets/Header/Header';
 import Main from './componets/Main/Main'
 import Footer from './componets/Footer/Footer'
+import userContext  from './context/userContext'; // contexto
+import { useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 
 function App() {
+  const [user, setUser] = useState("Guillermu"); // state para componente funcional
+
+  const login = (name) => setUser(name); // user = name. Ej. name="Guille" --> user="Guille"
+  const logout = () => setUser(""); // user = ""
+
+  const data = {
+    user,
+    login,
+    logout
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
+      <userContext.Provider value={data}>
+
         <Header/>
         <Main/>
+        </userContext.Provider>
+
         <Footer/>
       </BrowserRouter>
     </div>
--- src/componets/Main/Home/Home.jsx
+++ src/componets/Main/Home/Home.jsx
@@ -1,11 +1,34 @@
 import React, { Component } from "react";
 import Button from "@mui/material/Button";
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import '../../../styles/styles.css'
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import userContext from "../../../context/userContext"; // contexto
 
+import "../../../styles/styles.css";
 
 class Home extends Component {
+  static contextType = userContext; //Contexto desde JS con clases
+  constructor(props) {
+    super(props);
+    this.username = React.createRef();
+    this.state = {
+      username: "",
+    };
+  }
+
+  sendName = () => {
+    const { login } = this.context; // Consume contexto desde JS
+    login(this.state.username); 
+    alert("Nombre enviado: " + this.state.username);
+    this.username.current.value = "";
+    this.setState({ username: "" });
+  };
+
+  handleChange = () => {
+    const username = this.username.current.value; //Leer campo por referencia
+    this.setState({ username });
+  };
+
   render() {
     return (
       <div className="home-container">
@@ -13,23 +36,26 @@ class Home extends Component {
         <Box
           component="form"
           sx={{
-            '& > :not(style)': { m: 1, width: '25ch' },
+            "& > :not(style)": { m: 1, width: "25ch" },
           }}
           noValidate
           autoComplete="off"
         >
           <TextField
             id="standard-basic"
-            label="Introduce tú nombre"
+            type="text"
+            inputRef={this.username}
+            onChange={this.handleChange}
+            label="Introduce tu nombre"
             variant="standard"
             className="form-textfield"
           />
           <Button
             variant="contained"
-            className="css-button-sliding-to-left--red"
-            onClick={""}
+            onClick={this.sendName}
+            disabled={!this.state.username}
           >
-            LOGIN
+            Login
           </Button>
         </Box>
       </div>
@@ -38,3 +64,36 @@ class Home extends Component {
 }
 
 export default Home;
+
+// class Home extends Component {
+//   render() {
+//     return (
+//       <div className="home-container">
+//         <h1>BIENVENIDO A NEWS-APP</h1>
+//         <Box
+//           component="form"
+//           sx={{
+//             '& > :not(style)': { m: 1, width: '25ch' },
+//           }}
+//           noValidate
+//           autoComplete="off"
+//         >
+//           <TextField
+//             id="standard-basic"
+//             label="Introduce tú nombre"
+//             variant="standard"
+//             className="form-textfield"
+//           />
+//           <Button
+//             variant="contained"
+//             className="css-button-sliding-to-left--red"
+//             onClick={""}
+//           >
+//             LOGIN
+//           </Button>
+//         </Box>
+//       </div>
+//     );
+//   }
+// }
+// export default Home;
